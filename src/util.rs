9227a295628d1b68c95133a8a87b146fe9551ff1
r41ngee <r41ngee@yandex.ru>
2026-06-25 23:10:56 +0300

    Remove unused as_bytes method from Rgba struct

--- src/util.rs
+++ src/util.rs
@@ -9,16 +9,6 @@ pub struct Rgba {
 }
 
 impl Rgba {
-    #[allow(unused)]
-    pub fn as_bytes(&self) -> [u8; 4] {
-        let mut result = [255u8; 4];
-        (result[0], result[1], result[2]) = (self.r, self.g, self.b);
-        if let Some(a) = self.a {
-            result[3] = a;
-        }
-        result
-    }
-
     pub fn new(r: u8, g: u8, b: u8, a: Option<u8>) -> Self {
         Self { r, g, b, a }
     }
