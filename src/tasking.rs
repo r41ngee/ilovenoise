3ad9ee45949af3e5a272119db0d1ff0f78f5b776
r41ngee <r41ngee@yandex.ru>
2026-06-28 16:48:05 +0300

    Refactor main logic and update dependencies for task handling

--- src/tasking.rs
+++ src/tasking.rs
@@ -1,4 +1,8 @@
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
 use serde::{Deserialize};
+
+use crate::save_image;
 
 #[derive(Debug, Deserialize)]
 struct TasksConfig {
@@ -20,6 +24,35 @@ pub struct PerlinConfig {
     pub octaves: Option<u32>,
     pub persistence: Option<f32>,
     pub lacunarity: Option<f64>,
+}
+
+impl TaskConfig {
+    pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let size = (self.width, self.height);
+
+        let rand_thr = ChaCha8Rng::seed_from_u64(self.seed.unwrap_or_else(||rand::random()));
+        let mut algorithm = crate::create_mode(rand_thr, size, &self)?;
+        let mut image = crate::image::Image::new(size);
+
+        algorithm.draw(&mut image);
+
+        save_image(&image, self.output.clone().unwrap_or("output.png".to_string()).as_str() )
+    }
+
+    pub fn from_args(args: &crate::cli::Cli) -> Self {
+        Self {
+            mode: args.algo.clone().unwrap_or_default(),
+            width: args.width,
+            height: args.height,
+            output: args.output_path.clone(),
+            seed: args.seed,
+            perlin: (args.algo.as_deref() == Some("perlin")).then(|| PerlinConfig {
+                octaves: args.octaves,
+                persistence: args.persistence,
+                lacunarity: args.lacunarity,
+            }),
+        }
+    }
 }
 
 pub fn load_tasks(path: &str) -> Result<Vec<TaskConfig>, Box<dyn std::error::Error>> {
